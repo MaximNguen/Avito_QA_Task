fd9c44ee42c74347b802dd3bc2430ec72c5c31b8
Tutorial <foditoon1@gmail.com>
2025-11-25 22:10:09 +0400

    fix double classes and add 1 more negative test

--- tests/test_api_negative.py
+++ tests/test_api_negative.py
@@ -4,7 +4,9 @@ import allure
 from endpoints.endpoint_factory import EndpointFactory
 from utils.consts import Payloads
 
-class TestAPINegative:
+@allure.feature("Сервис от Авито")
+@allure.story("API тестирование ручек (4 на данный момент) - Негативные тест-кейсы")
+class TestCRUDNegative:
 
     @classmethod
     def setup_class(cls):
@@ -28,14 +30,20 @@ class TestAPINegative:
         delete_object = api_factory.delete_endpoint()
         delete_object.delete_by_id(object_id)
 
-    @allure.feature("Сервис от Авито")
-    @allure.story("API тестирование ручек (4 на данный момент) - Негативные тест-кейсы")
-    class TestCRUDNegative:
-        @allure.title("Не можем создать новое объявление - негативные тест-кейс №1")
-        def test_create_object_negative(self, api_factory):
-            with allure.step("Создаем объявление с помощью Page Object"):
-                new_object = api_factory.create_endpoint()
-                new_object.new_object(Payloads.create_payload_wrong)
+    @allure.title("Не можем создать новое объявление - негативный тест-кейс №1")
+    def test_create_object_negative(self, api_factory):
+        with allure.step("Создаем объявление с помощью Page Object"):
+            new_object = api_factory.create_endpoint()
+            new_object.new_object(Payloads.create_payload_wrong)
 
-            with allure.step("Проверяем статус кода"):
-                new_object.check_status_code(400)
+        with allure.step("Проверяем статус кода"):
+            new_object.check_status_code(400)
+
+    @allure.title("Не получаем созданное объявление - негативный тест-кейс №2")
+    def test_get_object(self, api_factory, test_object_id):
+        with allure.step("Получаем айди объявления"):
+            get_object = api_factory.get_endpoint()
+            get_object.get_object_by_id(test_object_id)
+
+        with allure.step("Достоверимся, что получили объявление"):
+            get_object.check_status_code(200)
--- tests/test_api_positive.py
+++ tests/test_api_positive.py
@@ -4,7 +4,9 @@ import allure
 from endpoints.endpoint_factory import EndpointFactory
 from utils.consts import Payloads
 
-class TestAPIPositive:
+@allure.feature("Сервис от Авито")
+@allure.story("API тестирование ручек (4 на данный момент) - Позитивные тест-кейсы")
+class TestCRUD:
 
     @classmethod
     def setup_class(cls):
@@ -28,41 +30,38 @@ class TestAPIPositive:
         delete_object = api_factory.delete_endpoint()
         delete_object.delete_by_id(object_id)
 
-    @allure.feature("Сервис от Авито")
-    @allure.story("API тестирование ручек (4 на данный момент) - Позитивные тест-кейсы")
-    class TestCRUD:
-        @allure.title("Создаем новое объявление - позитивный тест-кейс №1")
-        def test_create_object(self, api_factory):
-            with allure.step("Создаем объявление с помощью Page Object"):
-                new_object = api_factory.create_endpoint()
-                new_object.new_object(Payloads.create_payload)
+    @allure.title("Создаем новое объявление - позитивный тест-кейс №1")
+    def test_create_object(self, api_factory):
+        with allure.step("Создаем объявление с помощью Page Object"):
+            new_object = api_factory.create_endpoint()
+            new_object.new_object(Payloads.create_payload)
 
-            with allure.step("Проверяем статус кода"):
-                new_object.check_status_code(200)
+        with allure.step("Проверяем статус кода"):
+            new_object.check_status_code(200)
 
-        @allure.title("Получаем созданное объявление - позитивный тест-кейс №2")
-        def test_get_object(self, api_factory, test_object_id):
-            with allure.step("Получаем айди объявления"):
-                get_object =  api_factory.get_endpoint()
-                get_object.get_object_by_id(test_object_id)
+    @allure.title("Получаем созданное объявление - позитивный тест-кейс №2")
+    def test_get_object(self, api_factory, test_object_id):
+        with allure.step("Получаем айди объявления"):
+            get_object =  api_factory.get_endpoint()
+            get_object.get_object_by_id(test_object_id)
 
-            with allure.step("Достоверимся, что получили объявление"):
-                get_object.check_status_code(200)
+        with allure.step("Достоверимся, что получили объявление"):
+            get_object.check_status_code(200)
 
-        @allure.title("Получаем объявления определенного продавца - позитивный тест-кейс №3")
-        def test_get_list_object(self, api_factory):
-            with allure.step("Получаем айди продавца"):
-                get_object = api_factory.get_all_endpoint()
-                get_object.get_objects_by_id(1)
+    @allure.title("Получаем объявления определенного продавца - позитивный тест-кейс №3")
+    def test_get_list_object(self, api_factory):
+        with allure.step("Получаем айди продавца"):
+            get_object = api_factory.get_all_endpoint()
+            get_object.get_objects_by_id(1)
 
-            with allure.step("Достоверимся, что получили список объявлений"):
-                get_object.check_status_code(200)
+        with allure.step("Достоверимся, что получили список объявлений"):
+            get_object.check_status_code(200)
 
-        @allure.title("Получаем статистику объявление - позитивный тест-кейс №4")
-        def test_get_stats_object(self, api_factory, test_object_id):
-            with allure.step("Получаем айди объявления"):
-                get_object = api_factory.get_stat_endpoint()
-                get_object.action(test_object_id)
+    @allure.title("Получаем статистику объявление - позитивный тест-кейс №4")
+    def test_get_stats_object(self, api_factory, test_object_id):
+        with allure.step("Получаем айди объявления"):
+            get_object = api_factory.get_stat_endpoint()
+            get_object.action(test_object_id)
 
-            with allure.step("Достоверимся, что получили объявление"):
-                get_object.check_status_code(200)
+        with allure.step("Достоверимся, что получили объявление"):
+            get_object.check_status_code(200)
